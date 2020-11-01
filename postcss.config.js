082382c4e64c7fd1bb12f6c91ff46d0f0b4962b0
Shyam Bahety <bahetyshyam@gmail.com>
2020-11-01 14:17:03 +0530

    Added purgecss for tsx and ts files in config

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,20 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: [
-      './src/**/*.jsx',
-      './src/**/*.js',
-      './public/index.html'
-    ],
-    css: ['./src/assets/tailwind.css'],
-    // Include any special characters you're using in this regular expression
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-  })
-  
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: [
+    "./src/**/*.tsx",
+    "./src/**/*.ts",
+    "./src/**/*.jsx",
+    "./src/**/*.js",
+    "./public/index.html",
+  ],
+  css: ["./src/assets/tailwind.css"],
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
 
 module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-        ...process.env.NODE_ENV === 'production'
-            ? [purgecss]
-            : []
-    ],
+  plugins: [
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
 };
